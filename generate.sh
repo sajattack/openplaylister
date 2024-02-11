4976d507b4ff64ec270f8fc9b1f7c55a8c984656
Paul Sajna <hello@paulsajna.com>
2024-02-10 16:26:24 -0800

    generate using the train subset (the largest one)

--- generate.sh
+++ generate.sh
@@ -1,2 +1,2 @@
 seed=`od -A n -l -N 4 /dev/urandom | tr -d ' '`
-fairseq-interactive --path checkpoints/brainz/checkpoint_last.pt data-bin/brainz --task language_modeling --skip-invalid-size-inputs-valid-test --seed $seed --prefix-size 10 --sampling --nbest 1 --beam 1 --min-len 100
+fairseq-interactive --path checkpoints/brainz/checkpoint_last.pt data-bin/brainz --task language_modeling --skip-invalid-size-inputs-valid-test --seed $seed --prefix-size 10 --sampling --nbest 1 --beam 1 --min-len 100 --gen-subset train
